b94bc807025b62447494144ae81be7136e28433a
kareem Magdy <kareemmagdy1@yahoo.com>
2021-01-21 15:20:52 +0200

    initial setup4

---
+++ Models/User.js
@@ -0,0 +1,10 @@
+const mongoose=require('mongoose')
+
+const UserSchema=mongoose.Schema;
+
+const UserSchema=new Schema({
+
+
+})
+
+model.exports=mangose.model('User',UserSchema)
--- app.js
+++ app.js
@@ -1,2 +1,12 @@
 const express = require('express');
 const app = express();
+const userRoutes=require('/Routes/User')
+
+app.use(express.urlencoded({extended:false}))
+
+app.use(userRoutes)
+app.use('/',(req,res,nxt)=>{
+    console.log('yes')
+    return res.send('yes')
+})
+app.listen(3000)
